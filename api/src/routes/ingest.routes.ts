6eb02548c51ffad21974bd381fde09bb3beac39d
Mohak Chauhan <mohak46812@gmail.com>
2026-01-24 14:03:25 +0530

    remove: unwanted controllers & routes

--- api/src/routes/ingest.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { ingestText } from "../controllers/ingest.controller.js";
-
-const router = express.Router();
-
-router.post('/', ingestText)
-
-export {router as ingestRouter}
