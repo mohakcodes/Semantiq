723f6bf98e40790b697c8191583d29ff5554494d
Mohak Chauhan <mohak46812@gmail.com>
2026-02-05 17:54:59 +0530

    Revert "fix(api): correct ESM import paths for Node runtime"
    This reverts commit 75a15c6ade5ee3ab8e0dd46c42615b74a7eafa8f.

--- api/src/routes/analyze.routes.ts
+++ api/src/routes/analyze.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { analyzeText } from "../controllers/analyze.controller.js";
+import { analyzeText } from "../controllers/analyze.controller";
 
 const router = express.Router();
 router.post("/", analyzeText);
